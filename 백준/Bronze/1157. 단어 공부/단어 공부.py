3839f7cc552d1f7126a0dc69d5c3919a26739811
jylee9018 <31359740+jylee9018@users.noreply.github.com>
2025-04-21 23:46:29 +0900

    [Bronze I] Title: 단어 공부, Time: 188 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,32 @@
+# word = input().upper()
+
+# word_count = dict()
+# for i in range(len(word)):
+#     if word[i] not in word_count:
+#         word_count[word[i]] = 0
+#     word_count[word[i]] += 1
+
+# answer = []
+# max_count_value = max(word_count.values())
+# for c in word_count.keys():
+#     if word_count[c] == max_count_value:
+#         answer.append(c)
+
+# if len(answer) > 1:
+#     print("?")
+# else:
+#     print(answer[0])
+
+#-----
+
+# dict.get()을 활용하여 보다 간단히 카운트가 가능하다.
 word = input().upper()
+word_count = {}
 
-word_count = dict()
-for i in range(len(word)):
-    if word[i] not in word_count:
-        word_count[word[i]] = 0
-    word_count[word[i]] += 1
+for c in word:
+    word_count[c] = word_count.get(c, 0) + 1
 
-answer = []
-max_count_value = max(word_count.values())
-for c in word_count.keys():
-    if word_count[c] == max_count_value:
-        answer.append(c)
+max_count = max(word_count.values())
+common = [k for k, v in word_count.items() if v == max_count]
 
-if len(answer) > 1:
-    print("?")
-else:
-    print(answer[0])
+print("?" if len(common) > 1 else common[0])
