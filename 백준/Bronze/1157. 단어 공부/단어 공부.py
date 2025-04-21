64b30268c913a4be27b576a71f7918baa9aa3b6a
jylee9018 <31359740+jylee9018@users.noreply.github.com>
2025-04-21 23:49:09 +0900

    [Bronze I] Title: 단어 공부, Time: 112 ms, Memory: 36072 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+word = input().upper()
+
+count = Counter(word)
+most_common = count.most_common()
+
+if len(most_common) > 1 and most_common[0][1] == most_common[1][1]:
+    print("?")
+else:
+    print(most_common[0][0])
